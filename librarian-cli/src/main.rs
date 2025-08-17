d43d040af1bb5faaef313822cf3619534f754431
syedyaseen <s.syedyaseen.s@gmail.com>
2025-08-17 12:19:39 +0100

    init

---
+++ librarian-cli/src/main.rs
@@ -0,0 +1,38 @@
+use color_eyre::eyre::Result;
+use crossterm::event::{self, Event, KeyCode, KeyEvent, KeyEventKind};
+use ratatui::{
+    DefaultTerminal, Frame,
+    buffer::Buffer,
+    layout::Rect,
+    style::Stylize,
+    symbols::border,
+    text::{Line, Text},
+    widgets::{Block, Paragraph, Widget},
+};
+
+fn main() -> Result<()> {
+    color_eyre::install()?;
+    let mut terminal = ratatui::init();
+    let result = run(terminal);
+    ratatui::restore();
+    result
+}
+
+fn run(mut terminal: DefaultTerminal) -> Result<()> {
+    loop {
+        terminal.draw(render)?;
+        if let Event::Key(key) = event::read()? {
+            match key.code {
+                event::KeyCode::Esc => {
+                    break;
+                }
+                _ => {}
+            }
+        }
+    }
+    Ok(())
+}
+
+fn render(frame: &mut Frame) {
+    Paragraph::new("Hello from application").render(frame.area(), frame.buffer_mut());
+}
